3056e68783caf0c9a2f3bde9a9553e552c0fbf22
wguzc <113739728+wguzc@users.noreply.github.com>
2023-10-24 23:42:01 -0500

    lab 8 js

--- lab8.js
+++ lab8.js
@@ -1,4 +1,23 @@
-
 function getDataFromForm() {
-  alert("it worked!");
+    const firstName = document.getElementById("fname").value;
+    const lastName = document.getElementById("lname").value;
+    runAjax(firstName, lastName);
+}
+
+function runAjax(fname, lname) {
+    const xhr = new XMLHttpRequest();
+    const url = "./ajax.php"; // Adjust the path to match your server setup
+    const params = `fname=${fname}&lname=${lname}`;
+
+    xhr.open("GET", `${url}?${params}`, true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            if (typeof xhr.responseText === "string") {
+                document.getElementById("responseString").textContent = xhr.responseText;
+            }
+        }
+    };
+
+    xhr.send();
 }
